91d28329879eda0234e03bc89bf9db72b935efe3
jmateo95 <jmateo95@gmail.com>
2023-05-16 04:59:05 +0000

    cambios

--- script.sh
+++ script.sh
@@ -20,8 +20,8 @@ case $option in
     cd /home/Interpackage/InterPackage-Resources && git pull origin main && mvn clean install -DskipTests
     echo "Resultado de Resources: $?"
 
-    docker stop docker-interpackage-service-resource-1
-    docker rm docker-interpackage-service-resource-1
+    docker stop interPackage-docker-interpackage-service-resource-1
+    docker rm interPackage-docker-interpackage-service-resource-1
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-resource
     ;;
   1)
@@ -29,8 +29,8 @@ case $option in
     cd /home/Interpackage/InterPackage-Discovery-Server && git pull origin main && mvn clean install -DskipTests
     echo "Resultado de Discover: $?"
 
-    docker stop docker-interpackage-service-discovery-1
-    docker rm docker-interpackage-service-discovery-1
+    docker stop interPackage-docker-interpackage-service-discovery-1
+    docker rm interPackage-docker-interpackage-service-discovery-1
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-discovery
     ;;
   2)
@@ -38,8 +38,8 @@ case $option in
     cd /home/Interpackage/InterPackage-Gateway && git pull origin master && mvn clean install -DskipTests
     echo "Resultado de Gateway: $?"
 
-    docker stop docker-interpackage-service-gateway-1
-    docker rm docker-interpackage-service-gateway-1
+    docker stop interPackage-docker-interpackage-service-gateway-1
+    docker rm interPackage-docker-interpackage-service-gateway-1
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-gateway
     ;;
   3)
@@ -47,8 +47,8 @@ case $option in
     cd /home/Interpackage/InterPackage-Notification && git pull origin main && mvn clean install -DskipTests
     echo "Resultado de Notification: $?"
 
-    docker stop docker-interpackage-service-notification-1
-    docker rm docker-interpackage-service-notification-1
+    docker stop interPackage-docker-interpackage-service-notification-1
+    docker rm interPackage-docker-interpackage-service-notification-1
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-notification
     ;;
   4)
@@ -56,8 +56,8 @@ case $option in
     cd /home/Interpackage/InterPackage-Payments && git pull origin main && mvn clean install -DskipTests
     echo "Resultado de Payments: $?"
 
-    docker stop docker-interpackage-service-payment-1
-    docker rm docker-interpackage-service-payment-1
+    docker stop interPackage-docker-interpackage-service-payment-1
+    docker rm interPackage-docker-interpackage-service-payment-1
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-payment
     ;;
   5)
@@ -65,8 +65,8 @@ case $option in
     cd /home/Interpackage/InterPackage-Tracking && git pull origin main && mvn clean install -DskipTests
     echo "Resultado de Tracking: $?"
 
-    docker stop docker-interpackage-service-tracking-1
-    docker rm docker-interpackage-service-tracking-1
+    docker stop interPackage-docker-interpackage-service-tracking-1
+    docker rm interPackage-docker-interpackage-service-tracking-1
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-tracking
     ;;
   6)
@@ -74,8 +74,8 @@ case $option in
     cd /home/Interpackage/InterPackage-Users && git pull origin master && mvn clean install -DskipTests
     echo "Resultado de Users: $?"
 
-    docker stop docker-interpackage-service-users-1
-    docker rm docker-interpackage-service-users-1
+    docker stop interPackage-docker-interpackage-service-users-1
+    docker rm interPackage-docker-interpackage-service-users-1
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-users
     ;;
 
@@ -84,8 +84,8 @@ case $option in
     cd /home/Interpackage/InterPackage-Cards && git pull origin main && mvn clean install -DskipTests
     echo "Resultado de Cards: $?"
 
-    docker stop docker-interpackage-service-cards-1
-    docker rm docker-interpackage-service-cards-1
+    docker stop interPackage-docker-interpackage-service-cards-1
+    docker rm interPackage-docker-interpackage-service-cards-1
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-cards
     ;;
 
@@ -94,8 +94,8 @@ case $option in
     cd /home/Interpackage/InterPackage-Package && git pull origin master && mvn clean install -DskipTests
     echo "Resultado de Package: $?"
 
-    docker stop docker-interpackage-service-package-1
-    docker rm docker-interpackage-service-package-1
+    docker stop interPackage-docker-interpackage-service-package-1
+    docker rm interPackage-docker-interpackage-service-package-1
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-package
     ;;
 
@@ -128,25 +128,25 @@ case $option in
     cd /home/Interpackage/InterPackage-Package && git pull origin master && mvn clean install -DskipTests
     echo "Resultado de Package: $?"
 
-    docker stop docker-interpackage-service-resource-1
-    docker stop docker-interpackage-service-discovery-1
-    docker stop docker-interpackage-service-gateway-1
-    docker stop docker-interpackage-service-notification-1
-    docker stop docker-interpackage-service-payment-1
-    docker stop docker-interpackage-service-tracking-1
-    docker stop docker-interpackage-service-users-1
-    docker stop docker-interpackage-service-cards-1
-    docker stop docker-interpackage-service-package-1
+    docker stop interPackage-docker-interpackage-service-resource-1
+    docker stop interPackage-docker-interpackage-service-discovery-1
+    docker stop interPackage-docker-interpackage-service-gateway-1
+    docker stop interPackage-docker-interpackage-service-notification-1
+    docker stop interPackage-docker-interpackage-service-payment-1
+    docker stop interPackage-docker-interpackage-service-tracking-1
+    docker stop interPackage-docker-interpackage-service-users-1
+    docker stop interPackage-docker-interpackage-service-cards-1
+    docker stop interPackage-docker-interpackage-service-package-1
     
-    docker rm docker-interpackage-service-resource-1
-    docker rm docker-interpackage-service-discovery-1
-    docker rm docker-interpackage-service-gateway-1
-    docker rm docker-interpackage-service-notification-1
-    docker rm docker-interpackage-service-payment-1
-    docker rm docker-interpackage-service-tracking-1
-    docker rm docker-interpackage-service-users-1
-    docker rm docker-interpackage-service-cards-1
-    docker rm docker-interpackage-service-package-1
+    docker rm interPackage-docker-interpackage-service-resource-1
+    docker rm interPackage-docker-interpackage-service-discovery-1
+    docker rm interPackage-docker-interpackage-service-gateway-1
+    docker rm interPackage-docker-interpackage-service-notification-1
+    docker rm interPackage-docker-interpackage-service-payment-1
+    docker rm interPackage-docker-interpackage-service-tracking-1
+    docker rm interPackage-docker-interpackage-service-users-1
+    docker rm interPackage-docker-interpackage-service-cards-1
+    docker rm interPackage-docker-interpackage-service-package-1
 
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-resource
     cd /home/Interpackage/docker && docker-compose up -d --build interpackage-service-discovery
